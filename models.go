21403fffcd6711e2d2d3b6fb2f31b9131081664b
sankalp <sankalp.sharma@sofocle.com>
2017-06-06 15:19:30 +0530

    Made models for borrower, financial reports, compliance certificates, engineer, reserve reports.

---
+++ models.go
@@ -0,0 +1,57 @@
+package main
+
+
+
+type borrower struct {
+  Id string `json:"id"`
+  RegistratonID string `json:"registrationId"`
+  Name string `json:"name"`
+  Email float64 `json:"email"`
+
+}
+
+type financialReport struct {
+  Id string `json:"id"`
+  Date string `json:"date"`
+  LoanAmount float64 `json:"loanAmount"`
+  CreditPeriod int `json:"creditPeriod"`
+  Status string `json:"status"`
+}
+
+type complianceCertificate struct {
+  Id string `json:"id"`
+  Date string `json:"date"`
+  BorrowerId string `json:"borrowerId"`
+}
+
+type engineer struct {
+  Id string `json:"id"`
+  Name string `json:"name"`
+  RegistratonID string `json:"registrationId"`
+  Email string `json:"email"`
+
+}
+
+type reserveReport struct {
+  Id string `json:"id"`
+  Date string `json:"date"`
+  EngineerId string `json:"engineerId"`
+  BorrowerId string `json:"borrowerId"`
+  DevelopedCrude float64 `json:"developedCrude"`
+  UndevelopedCrude float64 `json:"undevelopedCrude"`
+}
+
+type loanPackage struct {
+  BorrowerId string `json:"borrowerId"`
+  FinancialReport financialReport `json:"financialReport"`
+  ComplianceCertificate complianceCertificate `json:"complianceCertificate"`
+  ReserveReport reserveReport `json:"reserveReport"`
+  AmountRequested string `json:"amountRequested"`
+  Status string `json:"status"`
+}
+
+type auditor struct {
+  Id string `json:"id"`
+  Name string `json:"name"`
+  FinancialReports []financialReport `json:"financialReport"`
+}
